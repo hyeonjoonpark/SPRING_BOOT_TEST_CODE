f3835348f2a86427348eb71737aaff46d0a75c12
hyunjoon park <105212959+hyeonjoonpark@users.noreply.github.com>
2024-05-12 23:46:58 +0900

    Arithmetic

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -2,16 +2,17 @@ package org.example;
 
 public class Calculator {
 
+//    if ("+".equals(operator)) {
+//      return operand1 + operand2;
+//    } else if ("-".equals(operator)) {
+//      return operand1 - operand2;
+//    } else if ("*".equals(operator)) {
+//      return operand1 * operand2;
+//    } else if ("/".equals(operator)) {
+//      return operand1 / operand2;
+//    }
+
   public static int calculate(int operand1, String operator, int operand2, int result) {
-    if ("+".equals(operator)) {
-      return operand1 + operand2;
-    } else if ("-".equals(operator)) {
-      return operand1 - operand2;
-    } else if ("*".equals(operator)) {
-      return operand1 * operand2;
-    } else if ("/".equals(operator)) {
-      return operand1 / operand2;
-    }
-    return 0;
+    return ArithmeticOperator.calculate(operand1, operator, operand2);
   }
 }
