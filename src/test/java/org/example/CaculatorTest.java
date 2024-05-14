664312cbb1f3a00dc4ed48bcad4831ad58b15bab
hyunjoon park <105212959+hyeonjoonpark@users.noreply.github.com>
2024-05-15 00:46:53 +0900

    FEAT: positive number test

--- src/test/java/org/example/CaculatorTest.java
+++ src/test/java/org/example/CaculatorTest.java
@@ -2,7 +2,6 @@ package org.example;
 
 import org.example.calculate.PositiveNumber;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -10,7 +9,6 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 /**
@@ -25,13 +23,17 @@ public class CaculatorTest {
   @MethodSource("formulaAndResult")
   @DisplayName("연산을 수행한다")
   void calculateTest(int operand1, String operator, int operand2, int result) {
-    int calculateResult = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
+    int calculateResult = Calculator.calculate(
+      new PositiveNumber(operand1),
+      operator,
+      new PositiveNumber(operand2)
+    );
 
     assertThat(calculateResult).isEqualTo(result);
   }
 
   private static Stream<Arguments> formulaAndResult() {
-    return  Stream.of(
+    return Stream.of(
       arguments(1, "+", 2, 3),
       arguments(1, "-", 2, -1),
       arguments(4, "*", 2, 8),
@@ -39,13 +41,4 @@ public class CaculatorTest {
     );
   }
 
-  @Test
-  @DisplayName("나눗셈에서 0으로 나누는 경우 예외를 발생시킨다")
-  void calculateExceptionTest() {
-    assertThatCode(() -> Calculator.calculate(
-      new PositiveNumber(10), "/", new PositiveNumber(0))
-    )
-      .isInstanceOf(IllegalArgumentException.class)
-      .hasMessage("0으로 나눌 수 없습니다");
-  }
 }
